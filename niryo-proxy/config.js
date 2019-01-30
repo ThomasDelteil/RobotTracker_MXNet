f3a4ec33dddb2fa986d9d37ceee90c3051a1ffd9
Anton Chernov <mechernov@gmail.com>
2019-01-30 18:27:57 +0100

    Structurd niryo-proxy code for 2 arms

---
+++ niryo-proxy/config.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const rosLib = require('roslib')
+
+const LEFT_ARM_HOST = '10.10.1.16'
+const LEFT_ARM_PORT = 9090
+
+const RIGHT_ARM_HOST = '10.10.1.19'
+const RIGHT_ARM_PORT = 9090
+
+const MAX_LEFT = new rosLib.Vector3(0.225, -0.220, 0.1)
+const MAX_RIGHT = new rosLib.Vector3(0.225, 0.220, 0.1)
+
+const ROLL = 0.0
+const PITCH = 1.57
+const YAW = 1.67
+
+const SERVER_NAME = '/niryo_one/commander/robot_action'
+const ACTION_NAME = 'niryo_one_msgs/RobotMoveAction'
+
+module.exports = {
+    arms: {
+        left: {
+            name: 'left',
+            host: LEFT_ARM_HOST,
+            port: LEFT_ARM_PORT,
+            server_name: SERVER_NAME,
+            action_name: ACTION_NAME
+        },
+        right: {
+            name: 'right',
+            host: RIGHT_ARM_HOST,
+            port: RIGHT_ARM_PORT,
+            server_name: SERVER_NAME,
+            action_name: ACTION_NAME
+        }
+    },
+    positions: {
+        max_left: {
+            ...MAX_LEFT,
+            roll: ROLL,
+            pitch: PITCH,
+            yaw: YAW
+        },
+        max_right: {
+            ...MAX_RIGHT,
+            roll: ROLL,
+            pitch: PITCH,
+            yaw: YAW
+        }
+    }
+};
---
+++ niryo-proxy/tools.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const rosLib = require('roslib')
+const EventEmitter = require('events');
+
+class ArmConfig {
+    constructor(name, host, port, server_name, action_name) {
+        this.name = name
+        this.host = host
+        this.port = port
+        this.server_name = server_name
+        this.action_name = action_name
+    }
+
+    toString() {
+        return JSON.stringify(this)
+    }
+}
+
+class Arm extends EventEmitter {
+    constructor(config) {
+        super()
+        this.config = config
+    }
+
+    init() {
+        const url = `ws://${this.config.host}:${this.config.port}`
+
+        console.log(`${this.config.name}: Initializing for url: ${url}`)
+
+        this.ros = new rosLib.Ros({
+            url: url
+        })
+
+        let that = this
+
+        // If there is an error on the backend, an 'error' emit will be emitted.
+        this.ros.on('error', function (error) {
+            console.log(`${that.config.name}: error: ${error}`)
+            //that.emit('error', error)
+        })
+
+        // Find out exactly when we made a connection.
+        this.ros.on('connection', function () {
+            console.log(`${that.config.name}: connected`)
+            that.emit('connection')
+        })
+
+        this.ros.on('close', function () {
+            console.log(`${that.config.name}: disconnected`)
+            //that.emit('close')
+        })
+
+        this.client = new rosLib.ActionClient({
+            ros: this.ros,
+            serverName: this.config.server_name,
+            actionName: this.config.action_name
+        })
+    }
+
+    move_pose(pos) {
+        console.log(`${this.config.name}: move_pose`)
+
+        const command = {
+            'cmd_type': 2,
+            'position': {
+                'x': pos.x,
+                'y': pos.y,
+                'z': pos.z,
+            },
+            'rpy': {
+                'roll': pos.roll,
+                'pitch': pos.pitch,
+                'yaw': pos.yaw,
+            }
+        }
+
+        let that = this
+
+        // Create a goal.
+        var goal = new rosLib.Goal({
+            actionClient: this.client,
+            goalMessage: new rosLib.Message({ 'cmd': command })
+        });
+
+        goal.on('feedback', function (feedback) {
+            console.log(`${that.config.name}: feedback: ${feedback.sequence}`)
+            that.emit('feedback', feedback)
+        });
+
+        goal.on('result', function (result) {
+            console.log(`${that.config.name}: result: ${result.sequence}`)
+            that.emit('result', result)
+        });
+
+        goal.on('timeout', function () {
+            console.log(`${that.config.name}: timeout`)
+            that.emit('timeout')
+        });
+
+        // Send the goal to the action server.
+        goal.send(1000);
+
+        console.log(`${this.config.name}: move_pose: goal sent`)
+    }
+
+    describe() {
+        console.log(`${this.config.name}: I'm an arm with config: ${this.config}`)
+    }
+}
+
+module.exports = {
+    Arm: Arm,
+    ArmConfig: ArmConfig
+};
